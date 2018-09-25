dd455bec4066282036c65cb2a965560bb676dbae
Yu Watanabe <yunabe.public@gmail.com>
2018-09-25 19:32:30 +0900

    Add a test case to reproduce #1

--- issues_test.go
+++ issues_test.go
@@ -1,15 +1,15 @@
 package easycsv
 
 import (
-	"bytes"
-	"reflect"
+	"strings"
 	"testing"
 	"time"
 )
 
 func TestIssue1(t *testing.T) {
 	// https://github.com/yunabe/easycsv/issues/1
-	r := NewReader(bytes.NewBufferString("name,birthday\nAlice,1980-12-30\nBob,1975-06-09"), Option{
+	// TODO(yunabe): Fix this bug.
+	r := NewReaderFile("testdata/issue1.csv", Option{
 		Decoders: map[string]interface{}{
 			"date": func(s string) (time.Time, error) {
 				return time.Parse("2006-01-02", s)
@@ -26,16 +26,12 @@ func TestIssue1(t *testing.T) {
 		names = append(names, entry.Name)
 		births = append(births, entry.Birth.Format("2006/01/02"))
 	}
-	if err := r.Done(); err != nil {
-		t.Error(err)
-		return
+	expectedErr := "\"date\" is not defined"
+	err := r.Done()
+	if err == nil {
+		t.Errorf("Expected %v but got no error", expectedErr)
 	}
-	nameExpects := []string{"Alice", "Bob"}
-	birthExpects := []string{"1980/12/30", "1975/06/09"}
-	if !reflect.DeepEqual(names, nameExpects) {
-		t.Errorf("Expected %v but got %v", nameExpects, names)
-	}
-	if !reflect.DeepEqual(births, birthExpects) {
-		t.Errorf("Expected %v but got %v", birthExpects, births)
+	if !strings.Contains(err.Error(), expectedErr) {
+		t.Errorf("expected an error with %q but got %q", expectedErr, err)
 	}
 }
